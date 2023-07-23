f17962a6d95d318f6f738391d94195bc04d1e440
jjanmo <jjanmo07@hanmail.net>
2023-07-23 13:28:50 +0900

    fix: resolve server routing error and cors

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const jsonServer = require('json-server');
 const auth = require('json-server-auth');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const router = jsonServer.router('db.json');
+const cors = require('cors');
+
 const port = process.env.PORT || 3000;
 
-server.use(middlewares);
+server.use(cors());
 server.use(auth);
+server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`json-server listening on ${port}`);
+});
